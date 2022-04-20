53c11eb5812f8fe3d70a04946b0671fe142496ca
Neeko <brossard.nicolas09@gmail.com>
2022-04-20 14:40:34 +0200

    add Title

--- container/title.jsx
+++ container/title.jsx
@@ -5,21 +5,22 @@ import { Text } from '@rneui/themed';
 export default () => {
   return (
     <View style={styles.contentView}>
-      <Logo width="28%" height="28%" viewBox="0 0 185 185" />
-      <Text style={styles.titleText}> Vos rendez-vous</Text>
+      <Logo width="32px" height="32px" viewBox="0 0 185 185" />
+      <Text style={styles.titleText}>Vos rendez-vous</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
   contentView: {
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    flexWrap: 'wrap',
-    alignContent: 'stretch',
   },
-
   titleText: {
-    fontSize: '1.6em',
+    fontSize: 30,
     fontWeight: 'bold',
+    color: '#393E40',
+    paddingLeft: 5,
+    includeFontPadding: true,
   },
 });
--- layout/loyaltycards.jsx
+++ layout/loyaltycards.jsx
@@ -1,19 +1,18 @@
 import { View, StyleSheet } from 'react-native';
-import Title from '../container/Title';
-import SearchBar from '../components/SearchBar';
+import Title from './../container/title';
+import SearchBar from './../components/searchbar';
 
 export default () => {
-	return (
-		<View style={styles.contentView}>
-			<Title/>
-			<SearchBar/>
-		</View>
-	);
+  return (
+    <View style={styles.contentView}>
+      <Title />
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-	contentView: {
-		flex: 1,
-		flexDirection:'column',
-		marginTop:"5%"
-	},
+  contentView: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: '10%',
+  },
 });

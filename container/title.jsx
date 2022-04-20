0db2e2e6f691c967adf000f254ceea129e8c0d48
Guillaume Charpentier <guillaume.charpentier@imie.fr>
2022-04-20 11:10:49 +0200

    Modification container

--- container/title.jsx
+++ container/title.jsx
@@ -1,25 +1,25 @@
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Logo from '../components/Logo';
-import {Text} from '@rneui/themed';
+import { Text } from '@rneui/themed';
 
 export default () => {
-	return (
-		<View style={styles.contentView}>
-			<Logo width="28%" height="28%" viewBox="0 0 185 185"/>
-			<Text style={styles.titleText}> Vos rendez-vous</Text>
-		</View>
-	);
+  return (
+    <View style={styles.contentView}>
+      <Logo width="28%" height="28%" viewBox="0 0 185 185" />
+      <Text style={styles.titleText}> Vos rendez-vous</Text>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-	contentView: {
-		flexDirection: "row",
-		alignItems:"center",
-		flexWrap:"wrap",
-		alignContent:"stretch"
-	},
+  contentView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    alignContent: 'stretch',
+  },
 
-	titleText: {
-		fontSize: '1.6em',
-		fontWeight: 'bold',
-	},
-})
+  titleText: {
+    fontSize: '1.6em',
+    fontWeight: 'bold',
+  },
+});

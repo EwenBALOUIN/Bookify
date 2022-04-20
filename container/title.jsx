e7b8666bc1467a39f20454cf42e21de8812e78eb
Lucas Durand <Lucas.DurandPro@gmail.com>
2022-04-20 14:01:33 +0200

    Création page mes clients + title

--- container/title.jsx
+++ container/title.jsx
@@ -1,26 +1,25 @@
-import { View, StyleSheet } from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Logo from '../components/Logo';
-import { Text } from '@rneui/themed';
+import {Text} from '@rneui/themed';
 
 export default () => {
-  return (
-      <View style={styles.contentView}>
-        <Logo width="20%" height="20%" viewBox="0 0 185 185" />
-        <Text style={styles.titleText}>  Vos rendez-vous</Text>
-      </View>
-  );
+	return (
+		<View style={styles.contentView}>
+			<Logo width="28%" height="28%" viewBox="0 0 185 185"/>
+			<Text style={styles.titleText}> Vos rendez-vous</Text>
+		</View>
+	);
 };
 const styles = StyleSheet.create({
-  contentView: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  titleText: {
-    flex: 1,
-    fontSize: '1.6em',
-    fontWeight : 'bold'
-    
-  },
+	contentView: {
+		flexDirection: "row",
+		alignItems:"center",
+		flexWrap:"wrap",
+		alignContent:"stretch"
+	},
+
+	titleText: {
+		fontSize: '1.6em',
+		fontWeight: 'bold',
+	},
 })

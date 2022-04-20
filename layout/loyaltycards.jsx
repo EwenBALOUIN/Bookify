b49303a3cd2f98c3dc087717c6a8d838907e94b8
SniiKox <45195523+SniiKox@users.noreply.github.com>
2022-04-20 15:39:07 +0200

    Ajout des cards (#3)
    Co-authored-by: Guillaume Charpentier <guillaume.charpentier@imie.fr>

--- layout/loyaltycards.jsx
+++ layout/loyaltycards.jsx
@@ -1,12 +1,25 @@
 import { View, StyleSheet } from 'react-native';
 import Title from './../container/title';
 import SearchBar from './../components/SearchBar'
+import { Text } from '@rneui/themed';
 
 export default () => {
   return (
     <View style={styles.contentView}>
       <Title text="Vos clients" />
       <SearchBar/>
+	  <View style={styles.card}>
+	  	<Text style={styles.nameText}>Guillaume Charpentier</Text>
+		<Text style={styles.pointText}>116 Pts</Text>
+      </View>
+	  <View style={styles.card}>
+	  	<Text style={styles.nameText}>Guillaume Charpentier</Text>
+		<Text style={styles.pointText}>116 Pts</Text>
+      </View>
+	  <View style={styles.card}>
+	  	<Text style={styles.nameText}>Guillaume Charpentier</Text>
+		<Text style={styles.pointText}>116 Pts</Text>
+      </View>
     </View>
   );
 };
@@ -16,5 +29,26 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     paddingTop: '10%',
     backgroundColor: 'white',
+  },
+  card: {
+	flex:3,
+    display: 'flex',
+    borderRadius: '5%',
+    flexDirection: 'column',
+    margin: '15%',
+    backgroundColor: '#6C0263',
+    shadowColor: '#969696',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+	padding: '5%',
+  },
+  pointText: {
+	paddingTop: '30%',
+	paddingLeft: '75%',
+	color: "#ffffff"
+  },
+  nameText: {
+	color: "#ffffff"
   },
 });

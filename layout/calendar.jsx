85f3f191ca2175c129253f98c27796b52c2128ca
Neeko <brossard.nicolas09@gmail.com>
2022-04-20 13:18:36 +0200

    add footer

--- layout/calendar.jsx
+++ layout/calendar.jsx
@@ -1,20 +1,42 @@
-import { View, StyleSheet } from 'react-native';
-import Title from '../container/title';
-import Footer from '../container/footer';
-import { Text } from '@rneui/themed';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import IconFa from 'react-native-vector-icons/FontAwesome';
+import IconFeather from 'react-native-vector-icons/Feather';
+
+import Login from './login';
+import Signup from './signup';
+
+const CalendarScreen = () => {
+  return <Login />;
+};
+
+const CustomerScreen = () => {
+  return <Signup />;
+};
+
+const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
-    <View style={styles.contentView}>
-      {/* <Title />
-      <Text h2> Nicolas B est votre prochain client</Text> */}
-      <Footer />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Calendar') {
+              return <IconFa name="calendar" size={30} color={'black'} />;
+            } else if (route.name === 'Users') {
+              return <IconFeather name="users" size={30} color={'black'} />;
+            }
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })}
+      >
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="Users" component={CustomerScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  contentView: {
-    display: 'flex',
-    width: '100%',
-  },
-});
